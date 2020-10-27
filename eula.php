636a4c42c439170b8258c97a001caa478a1edcd4
AdityaKane2001 <64411306+AdityaKane2001@users.noreply.github.com>
2020-10-27 15:57:25 +0530

    Update eula.php

--- eula.php
+++ eula.php
@@ -9,13 +9,13 @@
       <h1 align="center">End User License Agreement (EULA) and Cookie Policy</h1>
 
       <p> In addition to the <a href="https://www.000webhost.com/terms-of-use">000Webhost Terms Of Service</a>
-         the following is the End User License agreement for CBusie Timer.<br>CBusie Timer collects the following information</p>
+         the following is the End User License agreement for ClockMe Timer.<br>ClockMe Timer collects the following information</p>
          <ul>
            <li>Your provided username</li>
            <li>Your browser and device information</li>
 
          </ul>
-         <p>CBusie Timer uses cookies such that these cookies expire after 30 days.The cookies are inert to and for all web activities except CBusie Timer.One can choose not to store any cookies de-selecting 'Keep Me Logged In' option</p>
+         <p>ClockMe Timer uses cookies such that these cookies expire after 30 days.The cookies are inert to and for all web activities except ClockMe Timer.One can choose not to store any cookies de-selecting 'Keep Me Logged In' option</p>
          <p><b>CBusie Timer and its creators (Aditya Kane and Tanvesh Chavan) take no legal or ethical responsibility for any loss (physical,monetary,mental) incurred to the user.We do not guarantee security from phishing and foreign attacks.
          The source code of CBusie Timer comes under the sole ownership of its creators (Aditya Kane and Tanvesh Chavan). The statistics provided by CBusie Timer are not to be considered for any evaluation purposes whatsoever.</b> </p>
     </div>
